a941f65646032cce7463d6fef69ec4c99681d1cc
Thales Emiliano <115033316+thaemi@users.noreply.github.com>
2022-10-04 15:41:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+from selenium import webdriver 
+import time 
+
+x
 
 refreshrate=10
 st.markdown("Boas vindas ao")
   
 st.title('data.(Anpocs)')
 st.text('protótipo 1.0')
+st.text('azzzaaaaaazzzz')
 
 
