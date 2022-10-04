da13c079110c80913f35726558dab3592ca08bc3
Thales Emiliano <115033316+thaemi@users.noreply.github.com>
2022-10-04 15:30:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ import numpy as np
 import plotly.express as px
 
 st.title('data.(Anpocs)')
+st.text('protótipo 1.0')
+
+st.markdown("Entre maio e julho, o **Brasil** jogou com 10 seleções mundiais! Acompanhe quem venceu!")
