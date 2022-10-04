7ad451f54664152265162cfe0429c5e06f8163d4
Thales Emiliano <115033316+thaemi@users.noreply.github.com>
2022-10-04 15:36:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-st.markdown("Entre maio e julho, o **Brasil** jogou com 10 seleções mundiais! Acompanhe quem venceu!")
+st.experimental_rerun 
+
+
+st.markdown("Boas vindas ao")
 st.title('data.(Anpocs)')
 st.text('protótipo 1.0')
 
