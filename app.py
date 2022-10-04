7e2f3e8ee738785cc252308b90958690154737d5
Thales Emiliano <115033316+thaemi@users.noreply.github.com>
2022-10-04 15:48:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-from streamlit_autorefresh import st_autorefresh
+
 st.markdown("Boas vindas ao")
 st.title('data.(Anpocs)')
 st.text('protótipo 1.0')
