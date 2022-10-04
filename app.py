378c51a0ec4edb4b5d08bf0a0f364b3db105642a
Thales Emiliano <115033316+thaemi@users.noreply.github.com>
2022-10-04 15:54:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import numpy as np
 import plotly.express as px
 
 st.markdown("Boas vindas ao")
-st.title('data.(Anpocs)')
+st.title('data.(Anpocs)!')
 st.text('protótipo 1.0')
 st.text('ola, ola, ola')
