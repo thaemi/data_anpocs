a950b70df0b976eddfe506b8e5929c267ac9dbb8
Thales Emiliano <115033316+thaemi@users.noreply.github.com>
2022-10-05 09:51:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,6 @@ selecao = st.sidebar.selectbox('', lista_dos_trabalhos)
 
 gol, gol2 = st.beta_columns([5,20])
 with gol
-  st.image('projeto.py/1.png')
+  st.image('1.png')
   with gol2:
     st.info('Logo oficial do data.(Anpocs)')
