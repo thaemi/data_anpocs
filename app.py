c84d77d11ae3c4587205aac5ed39e8e7142cb96b
Thales Emiliano <115033316+thaemi@users.noreply.github.com>
2022-10-05 10:36:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,22 +6,4 @@ import plotly.express as px
 st.markdown("Boas vindas ao")
 st.image("projeto.py/1.png")
 
-st.title('data.(Anpocs)!')
-
-st.write2('Acima, vocês vêem o nosso logotipo!')
-
-st.text('protótipo 1.0')
-st.text('ola, ola, ola')
-st.write('fale conosco bla bla bla')
-
-trabalhos = pd.read_excel('trabalhos.py.xlsx')
-
-lista_dos_trabalhos = trabalhos['Tema do GT'].unique()
-
-lista_dos_trabalhos = np.append(lista_dos_trabalhos, '')
-
-lista_dos_trabalhos.sort()
-
-selecao = st.sidebar.selectbox('', lista_dos_trabalhos)
-
 
