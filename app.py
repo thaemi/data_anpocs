90a1ccb26d67899f99cae0674ca2efca8af68821
Thales Emiliano <115033316+thaemi@users.noreply.github.com>
2022-10-05 19:40:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ selecao = st.sidebar.selectbox('', lista_dos_trabalhos)
 if selecao == 'Gênero, Trabalho e família':
   st.header("Esses são os trabalhos do tema 'Gênero, Trabalho e família' ")
 if selecao == 'Democracia e desigualdades':
-  st.header("Esses são os trabalhos do tema 'Democracia e desigualdades")
+  st.header("Esses são os trabalhos do tema 'Democracia e desigualdades' ")
