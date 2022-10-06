a044900bf96d8429e1f4f907fd334fba2e34673e
Thales Emiliano <115033316+thaemi@users.noreply.github.com>
2022-10-06 13:36:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ import plotly.express as px
 st.text(" Plataforma Teste para projeto 'data.(Anpocs).' ")
 st.text('protótipo 0.0.0.1')
 
-st.markdown("Boas vindas ao")
-st.image("projeto.py/data.png")
+st.markdown("Boas vindas ao") st.image("projeto.py/data.png")
 st.title('Trabalhos armazenados pelo data.(Anpocs).')
 
 trabalhos = pd.read_excel('trabalhos.py.xlsx')
