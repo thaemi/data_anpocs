337500c98cadaf4505907875beb4b12238857654
Thales Emiliano <115033316+thaemi@users.noreply.github.com>
2022-10-05 21:11:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-st.text(" Plataforma Teste para projeto 'data.(Anpocs)' ")
+st.text(" Plataforma Teste para projeto 'data.(Anpocs).' ")
 st.text('protótipo 0.0.0.1')
 
 st.markdown("Boas vindas ao")
 st.image("projeto.py/1.png")
-st.title(' Trabalhos armazenados pelo data.(Anpocs)')
+st.title('Trabalhos armazenados pelo data.(Anpocs)')
 
 
 
