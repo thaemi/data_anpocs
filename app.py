2b0376e3a94d07cb419ab36dc49579b9c306a50a
Thales Emiliano <115033316+thaemi@users.noreply.github.com>
2022-10-06 15:23:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ st.text('protótipo 0.0.0.1')
 
 st.title("Boas vindas ao")
 st.image("projeto.py/1.png", width=200)
-st.title('Trabalhos armazenados pelo')
-st.title('data.(Anpocs)')
+st.title('Trabalhos armazenados pelo data.(Anpocs)')
+
 trabalhos = pd.read_excel('trabalhos.py.xlsx')
 
 lista_dos_trabalhos = trabalhos['Tema do GT'].unique()
@@ -22,21 +22,24 @@ selecao = st.sidebar.selectbox('', lista_dos_trabalhos)
 
 if selecao == 'Gênero, Trabalho e família':
   st.header("Esses são os trabalhos do tema 'Gênero, Trabalho e família' ")
+  st.markdown("- As desigualdades nas narrativas de homens e mulheres no ABC paulista: gênero, família e trabalho")
+  st.markdown("- Antigos habitus, novos direitos: a persistente desigualdade no trabalho doméstico")
+  st.markdown("- Trabalho e família no campo da tecnologia da informação: as desigualdades persistem?")
   st.write("[link] (http://anpocs.org/index.php/encontros/papers/41-encontro-anual-da-anpocs/gt-30/gt13-17)")
-  
-  st.markdown("As desigualdades nas narrativas de homens e mulheres no ABC paulista: gênero, família e trabalho")
-  
-  
-  st.markdown("Antigos habitus, novos direitos: a persistente desigualdade no trabalho doméstico")
-  
-  st.markdown("Trabalho e família no campo da tecnologia da informação: as desigualdades persistem?")
   
 
 if selecao == 'O Rural no Brasil contemporâneo: questões teóricas e novos temas de pesquisa' :
-  st.header("Esses são os trabalhos do tema 'O Rural no Brasil contemporâneo: questões teóricas e novos temas de pesquisa' ")
- 
+  st.header("Esses são os trabalhos do tema 'O Rural no Brasil contemporâneo: questões teóricas e novos temas de pesquisa")
+  st.markdown("Dinâmica da população e produção de commodities: desigualdades socioespaciais no rural paulista")  
+  st.write("[link] (http://anpocs.org/index.php/encontros/papers/41-encontro-anual-da-anpocs/gt-30/gt19-26")
+  
 if selecao == 'Controles Democráticos: Instituições e participação na democracia contemporânea':
   st.header("Esses são os trabalhos do tema 'Controles Democráticos: Instituições e participação na democracia contemporânea' ")
+  
+  st.markdown("Instituições Participativas, Desigualdades Sociais e Democracia")
+  st.write("[link] (https://www.anpocs.com/index.php/papers-40-encontro-3/gt-31/gt07-16/11164-instituicoes-participativas-desigualdades-sociais-e-democracia/file")
+  st.mardown("Controles Democráticos: Instituições e participação na democracia contemporânea")
+  st.write("[link] (https://anpocs.com/index.php/papers-38-encontro/gt-1/gt08-1/8889-desigualdade-na-ampliacao-das-inovacoes-participativas-caracteristicas-dos-municipios-com-orcamentos-participativos-no-brasil/file")
 
 if selecao == 'As classes sociais no Brasil contemporâneo':
   st.header("Esses são os trabalhos do tema 'As classes sociais no Brasil contemporâneo' ")
